b170bfd478bfdaf6225ed4bfba721b43261ce4a0
Matthew Soh <matthewsoh.cw@gmail.com>
2017-01-06 16:12:08 +0800

    Fixed bugs in model declaration

--- server/newsly/api_server/models.py
+++ server/newsly/api_server/models.py
@@ -1,30 +1,31 @@
 from django.db import models
+import uuid
 
-class Client(models.model):
+class Client(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=256)
 
-class Stream(models.model):
+class Stream(models.Model):
     # TODO: Consider cascade model for stream deletion
-    stream = models.ForeignKey(Stream, on_delete=models.CASCADE)
+    stream = models.ForeignKey(Client, on_delete=models.CASCADE)
 
-class Card(model.model):
+class Card(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = model.CharField(max_length=256)
-    src = model.URLField()
-    img = model.URLField()
+    title = models.CharField(max_length=256)
+    src = models.URLField()
+    img = models.URLField()
     streams = models.ManyToManyField(Stream)
 
-class Decision(model.model):
+class Decision(models.Model):
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
     value = models.IntegerField()
 
-class Flag(model.model):
-    title = model.CharField(max_length=256)
+class Flag(models.Model):
+    title = models.CharField(max_length=256)
     cards = models.ManyToManyField(Card);
 
-class Tag(model.model):
-    title = model.CharField(max_length=256)
+class Tag(models.Model):
+    title = models.CharField(max_length=256)
     cards = models.ManyToManyField(Card)
 
 # A Client has many Stream, a Stream belongs to one Client
