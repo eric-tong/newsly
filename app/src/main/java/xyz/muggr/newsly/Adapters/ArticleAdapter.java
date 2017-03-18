8bb64e85c6e3d54e1ac7c07681204a1066a8fe40
eric-tong <erictong.designs@gmail.com>
2017-03-18 17:48:47 +0000

    Use CollapsingToolbar

--- app/src/main/java/xyz/muggr/newsly/Adapters/ArticleAdapter.java
+++ app/src/main/java/xyz/muggr/newsly/Adapters/ArticleAdapter.java
@@ -2,6 +2,7 @@ package xyz.muggr.newsly.Adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import xyz.muggr.newsly.R;
@@ -16,7 +17,12 @@ public class ArticleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int x = v.getId();
+            }
+        });
     }
 
     @Override
