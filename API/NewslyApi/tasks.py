870be127e3da93ef62b35d2dd5f90ea65777afae
eric-tong <erictong.designs@gmail.com>
2017-07-29 22:43:28 +0800

    Selective selection for card data

--- API/NewslyApi/tasks.py
+++ API/NewslyApi/tasks.py
@@ -8,8 +8,12 @@ def start():
     print('Start reddit interface')
 
     reddit_url = 'https://www.reddit.com/r/worldnews/.json?'
-    schedule.every(10).minutes.do(Downloader.download, reddit_url)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    try:
+        schedule.every(10).minutes.do(Downloader.download, reddit_url)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(60)
+    except:
+        start()
--- API/NewslyApi/views.py
+++ API/NewslyApi/views.py
@@ -11,7 +11,15 @@ def cards(request):
     if request.GET.get('refresh'):
         Downloader.download('https://www.reddit.com/r/worldnews/.json?limit=20')
 
-    reddit_articles = list(RedditArticle.objects.all())
+    reddit_articles = list(RedditArticle.objects.raw('SELECT '
+                                                     'reddit_id, '
+                                                     'reddit_id AS redditId, '
+                                                     'reddit_title AS redditTitle, '
+                                                     'article_domain AS articleDomain, '
+                                                     'article_top_image AS articleTopImage, '
+                                                     'reddit_flair AS redditFlair, '
+                                                     'reddit_created AS redditCreated '
+                                                     'FROM NewslyApi_redditarticle'))
 
     return HttpResponse(json.dumps(reddit_articles, default=json_default))
 
